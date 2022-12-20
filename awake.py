76189e371026d8b886ed640ab769f36d41639ac7
pykira-cpu <sainagaakhil@gmail.com>
2022-12-20 11:37:33 -0600

    comments to system awake script

--- awake.py
+++ awake.py
@@ -1,11 +1,12 @@
 import pyautogui, time, random
 
-
-print(pyautogui.size())
-
+print(pyautogui.size())  
+# output: screen resolution in the format(x,y)
 while True:
     
-	x = random.randrange(0,1440,1)
-	y = random.randrange(0, 900, 1)	
-	pyautogui.moveTo(x,y,duration = 5)
-	print(time.ctime(time.time()))
+	x = random.randrange(0,1440,1) # 1440 -> value of x
+	y = random.randrange(0, 900, 1)	# 990 -> value of y
+	pyautogui.moveTo(x,y,duration = 5) # moves cursor to randomized points generated every 5 seconds
+	print(time.ctime(time.time())) # displays string notion of currentTime
+ 
+ 
